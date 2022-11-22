a7b8f8a5401b537765cb63527d0c03fc09f24736
Darshan Chaudhari <2020bit029@sggs.ac.in>
2022-11-22 18:24:19 +0530

    second commit

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 # from .views import view_stud_info,view_admission,view_feedback,view_marks
 from home import views
-from django.conf.urls.defaults import *
 
 urlpatterns = [
     
@@ -12,5 +11,5 @@ urlpatterns = [
     path('feedback/',views.view_feedback,name="feedback"),
     path('view_data/', views.view_data ,name= 'view_data'),
     path('',views.index,name="index") ,
-
+    path('insert/', views.insert, name='insert'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,53 +1,5 @@
 from django.shortcuts import render
 from home.models import *
-from django_datatables_view.base_datatable_view import BaseDatatableView
-from django.utils.html import escape
-
-
-class OrderListJson(BaseDatatableView):
-    # The model we're going to show
-    model = Student
-
-    # define the columns that will be returned
-    columns = ['sname', 'reg_no', 'taluka', 'address', 'state']
-
-    # define column names that will be used in sorting
-    # order is important and should be same as order of columns
-    # displayed by datatables. For non-sortable columns use empty
-    # value like ''
-    order_columns = ['number', 'user', 'state', '', '']
-
-    # set max limit of records returned, this is used to protect our site if someone tries to attack our site
-    # and make it return huge amount of data
-    max_display_length = 500
-
-    def render_column(self, row, column):
-        # We want to render user as a custom column
-        if column == 'user':
-            # escape HTML for security reasons
-            return escape('{0} {1}'.format(row.customer_firstname, row.customer_lastname))
-        else:
-            return super(OrderListJson, self).render_column(row, column)
-
-    def filter_queryset(self, qs):
-        # use parameters passed in GET request to filter queryset
-
-        # simple example:
-        search = self.request.GET.get('search[value]', None)
-        if search:
-            qs = qs.filter(name__istartswith=search)
-
-        # more advanced example using extra parameters
-        filter_customer = self.request.GET.get('customer', None)
-
-        if filter_customer:
-            customer_parts = filter_customer.split(' ')
-            qs_params = None
-            for part in customer_parts:
-                q = Q(customer_firstname__istartswith=part) | Q(customer_lastname__istartswith=part)
-                qs_params = qs_params | q if qs_params else q
-            qs = qs.filter(qs_params)
-        return qs
 
 
 # Create your views here.
@@ -88,6 +40,16 @@ def view_data(request):
   student_data = Student.objects.all()
   student_marks = Marks.objects.all()
   student_feedback = Feedback.objects.all()
-  context = {'student' : student_data , 'marks' : student_marks , 'feedback' : student_feedback}
-  return render(request , 'view.html' , context)
+  return render(request , 'view.html' , {'student' : student_data , 'marks' : student_marks , 'feedback' : student_feedback})
 # Create your views here.
+
+
+def abt(request):
+  student_data = Student.objects.all()
+  return render(request ,'stu.html' ,{'student' : student_data} )
+
+def insert(request):
+    Student = Student(sname=request.POST['sname'], reg_no=request.POST['reg_no'], address=request.POST['address'] ,taluka=request.POST['taluka'] ,district=request.POST['district'] ,state=request.POST['state'] )
+    print("LOL")
+    Student.save()
+    return redirect('/')
